c5b0e2bdfa0d3c3c326c0b478cf44603344cc264
Gurjot Sidhu <gurjotsidhu4@gmail.com>
2019-11-29 21:49:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ var instruments = [];
 var instrul = instruments.length;
 function theBeatlesPlay(array1,array2){
   for(l=0;l < array1.length; l++){
-    return array1[l] + "plays" + array2[l];
+    music[]= array1[l] + "plays" + array2[l]
   }
 }
