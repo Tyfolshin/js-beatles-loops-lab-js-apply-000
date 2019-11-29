5f6b43c66110f73bb6e2a5fb1f1b0f0e4db49974
Gurjot Sidhu <gurjotsidhu4@gmail.com>
2019-11-29 22:03:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // add solution here
 function theBeatlesPlay(array1,array2){
   var music = [];
-  for(i=0;i < array1.length; i++){
+  for(var i=0;i < array1.length; i++){
     music.push(array1[i] + "plays" + array2[i]);
   }
   return music;
