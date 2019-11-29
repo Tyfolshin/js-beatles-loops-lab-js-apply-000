710db486a6c826f9d09b1669d756f691891fddf6
Gurjot Sidhu <gurjotsidhu4@gmail.com>
2019-11-29 21:40:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // add solution here
 var music = []
 var musiclen = music.length;
-function theBeatlesPlay([],[]){
+function theBeatlesPlay([music],[insruments]){
   for(l=0;l < musiclen; l++){
     
   }
