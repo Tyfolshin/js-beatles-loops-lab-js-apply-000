7e4c4671382734ca419edce97cb8f9e3c7c7b0e5
Gurjot Sidhu <gurjotsidhu4@gmail.com>
2019-11-29 21:55:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 // add solution here
 var music = [];
-var musiclen = music.length;
-var instruments = [];
-var instrul = instruments.length;
 function theBeatlesPlay(array1,array2){
   for(l=0;l < array1.length; l++){
-    music[]= array1[l] + "plays" + array2[l]
+    music[l]= array1[l] + "plays" + array2[l];
+    return music[l];
   }
 }
